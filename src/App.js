135d2fe0bf0157b24eaa66b8055b4951a4d52484
Isis van der Plas <isisvanderplas@hotmail.com>
2017-12-15 00:39:00 +0100

    add testing dependencies

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Title content="Hello, World"/>
+        <Title content="Evaluation Tool"/>
       </div>
     );
   }
