240abe689b93230febdfddb853f60704b56010ef
Isis van der Plas <isisvanderplas@hotmail.com>
2017-12-15 04:32:05 +0100

    adjust proxy in package.json

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { Student } = require('./models');
+const { Batch } = require('./models');
+
 
 let app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 3030
+const PORT = process.env.PORT || 3030;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express!')
-})
+  res.sendFile('./src/App.js');
+});
+
+app.get('/students', (req, res, next) => {
+  Student.find()
+    // Send the data in JSON format
+    .then((students) => res.json(students));
+    // Forward any errors to error handler
+    .catch((error) => next(error));
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
-})
+  console.log(`Server is listening on port ${PORT}`);
+});
